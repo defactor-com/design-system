929fc5cd89bcfe53851d89eef64d01ee793b57ce
Tiimie1 <tim.pangersic@gmail.com>
2026-03-20 15:31:57 +0100

    Updates

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import RWAStyleguide from "../RWAStyleguideQ22026.jsx";
-
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <RWAStyleguide />
-  </StrictMode>
-);
