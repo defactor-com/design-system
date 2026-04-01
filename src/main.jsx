d382be49696cdcc676e1a15e0af5127ecb8a9763
Tiimie1 <tim.pangersic@gmail.com>
2026-04-01 11:44:00 +0200

    init

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "../DEFACTOR-DESIGN_SYSTEM_final-v1.7.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
